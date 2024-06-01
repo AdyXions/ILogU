756d7e01e266fbed49b1c9c2f5188bbafc22576d
AdyXions <angelolukang.official@gmail.com>
2024-06-01 18:36:54 +0800

    update

--- src/layouts/components/Footer.vue
+++ src/layouts/components/Footer.vue
@@ -2,8 +2,6 @@
   <div class="h-100 d-flex align-center justify-space-between text-medium-emphasis">
     <!-- 👉 Footer: left content -->
     <span class="d-flex align-center">
-      &copy;
-      {{ new Date().getFullYear() }}
       Made With
       <VIcon
         icon="ri-heart-line"
--- src/pages/server/[id]/directories.vue
+++ src/pages/server/[id]/directories.vue
@@ -1,6 +1,14 @@
 <script setup>
 const router = useRouter()
 
+const headers = [
+  { title: 'File System', key: 'Filesystem' },
+  { title: '1K-Blocks', key: '1K-blocks' },
+  { title: 'Used', key: 'Used' },
+  { title: 'Available', key: 'Available' },
+  { title: 'Use %', key: 'Use%' },
+  { title: 'Mounted On', key: 'Mounted' },
+]
 </script>
 
 <template>
@@ -22,6 +30,20 @@ const router = useRouter()
           Directories
         </h2>
       </div>
+    </VCol>
+  </VRow>
+
+  <VRow>
+    <VCol>
+      <VCard>
+        <VCardText>
+          <VDataTable
+            :headers="headers"
+            :items-per-page="50"
+            class="text-no-wrap"
+          />
+        </VCardText>
+      </VCard>
     </VCol>
   </VRow>
 </template>
